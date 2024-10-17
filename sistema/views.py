2578555e0e838e78286d15e9a892a23c7f12fccb
MatheusMontP <matheusmontenegrofacul@gmail.com>
2024-10-17 15:25:27 -0300

    Parte de estoque semi-completa
    Falta implementar pequenas validações

--- sistema/views.py
+++ sistema/views.py
@@ -1,40 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Produtos
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def estoque(request):
+    #Isso assegura que o estoque sejá atualizado todas as vezes que alguem entrar na home!
     if request.method == 'GET':
         verprod = Produtos.objects.all()
         return render(request, 'estoque/estoque.html', {'verprod': verprod})
     
-def adicionar_estoque(request):
+@login_required
+def adicionar_produto(request):
+
     if request.method == 'GET':
-        verprod = {
-            'verprod': Produtos.objects.all()
-        }
-        return render(request, 'estoque/editar/adicionar.html', verprod)
+        #Aqui eu estou renderizando meu html
+        return render(request, 'estoque/editar/adicionar.html')
     else:
-        produto = Produtos(
+        #Primeiro eu estou faazendo uma verificação para assegurar que não tenha um produto com o mesmo nome, caso tenha, aparece um pop-up na tela
+        nome_prod = request.POST.get('nome')
+        verificar = Produtos.objects.filter(nome_prod=nome_prod).first()
+
+        if verificar:
+            return render(request, 'estoque/editar/adicionar.html', {'nao_add': True})
+        else:
+            produtos = Produtos(
             nome_prod=request.POST.get('nome'),
             vlr_prod=request.POST.get('valor'),
             qtnd_prod=request.POST.get('qntd'),
             entrada_prod=request.POST.get('data_entrada')
-        )
+            )
+            produtos.save()
+
+            return redirect('estoque')
+        
+@login_required
+def deletar_produto(request, id_prod):
+    #Estou buscando o produto pelo id, se nao existir, da erro 404
+    produto = get_object_or_404(Produtos, id_prod=id_prod)
+
+    if request.method == 'POST':
+        #Função para deletar o produto, produto esse que selecionei pelo id
+        produto.delete()
+        return redirect('estoque')  # Redireciona para o estoque
+
+    return render(request, 'estoque/editar/remover.html', {'produto': produto})
+
+@login_required
+def modificar_produto(request, id_prod):
+    #Isso aqui vai ver se o produto existe, se nao existir, retorna uma página 404
+    produto = get_object_or_404(Produtos, id_prod = id_prod)
+
+    if request.method == 'POST':
+        # Recebe os dados do formulário
+        produto.nome_prod = request.POST.get('nome')
+        produto.vlr_prod = request.POST.get('valor')
+        produto.qtnd_prod = request.POST.get('quantidade')
+        produto.entrada_prod = request.POST.get('data_entrada')
+
         produto.save()
         
-        if produto is None:
-            return render(request, 'estoque/editar/adicionar.html', {'nao_add': True})
-        else:
-            return render(request, 'estoque/editar/adicionar.html', {'add': True})
+        #Volta para a tela inicial
+        return redirect('estoque') 
 
-
-def remover_estoque(request):
-    if request.method == 'GET':
-        return render(request, 'estoque/editar/remover.html')
-
-def modificar_estoque(request):
-    if request.method == 'GET':
-        return render(request, 'estoque/editar/modificar.html')    
+    return render(request, 'estoque/editar/modificar.html', {'produto': produto})
