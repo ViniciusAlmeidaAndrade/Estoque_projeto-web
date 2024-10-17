74dc8751264484f62dc003ca674e8c80590ff23a
MatheusMontP <matheusmontenegrofacul@gmail.com>
2024-10-17 01:08:06 -0300

    att

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Produtos
 
 # Register your models here.
+
+@admin.register(Produtos)
+class ProdutosAdmin(admin.ModelAdmin):
+    list_display = ('id_prod', 'nome_prod', 'vlr_prod', 'qtnd_prod', 'entrada_prod')  # Adicione os campos que deseja exibir
+
--- sistema/views.py
+++ sistema/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from .models import Produtos
 
 # Create your views here.
 
 def estoque(request):
     if request.method == 'GET':
-        return render(request, 'estoque/estoque.html')
+        verprod = Produtos.objects.all()
+        return render(request, 'estoque/estoque.html', {'verprod': verprod})
     
 def adicionar_estoque(request):
     if request.method == 'GET':
-        return render(request, 'estoque/editar/adicionar.html')
+        verprod = {
+            'verprod': Produtos.objects.all()
+        }
+        return render(request, 'estoque/editar/adicionar.html', verprod)
+    else:
+        produto = Produtos(
+            nome_prod=request.POST.get('nome'),
+            vlr_prod=request.POST.get('valor'),
+            qtnd_prod=request.POST.get('qntd'),
+            entrada_prod=request.POST.get('data_entrada')
+        )
+        produto.save()
+        
+        if produto is None:
+            return render(request, 'estoque/editar/adicionar.html', {'nao_add': True})
+        else:
+            return render(request, 'estoque/editar/adicionar.html', {'add': True})
+
 
 def remover_estoque(request):
     if request.method == 'GET':
