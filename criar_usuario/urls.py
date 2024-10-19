7c247cc8a6284a48d5b7d0dca94b5a45ed041676
MatheusMontP <matheusmontenegrofacul@gmail.com>
2024-10-18 23:48:27 -0300

    Estoque e  criar usuarios done

--- criar_usuario/urls.py
+++ criar_usuario/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.criar_usuario, name = 'criar_usuario'),
+    path('lista', views.lista_user, name = 'lista'),
+    path('mudar_senha/<int:id>', views.mudar_senha, name = 'editar'), 
+    path('deletar/<int:id>', views.deletar_usuario, name = 'deletar'),   
+    path('mudar_informações/<int:id>', views.modificar_usuario, name = 'mudar_informações'),   
+
 ]
--- criar_usuario/views.py
+++ criar_usuario/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from rolepermissions.roles import assign_role
@@ -25,4 +25,51 @@ def criar_usuario(request):
             assign_role(user, 'tecnico')
             return render(request, 'cadastro.html', {'add': True})
 
+def lista_user(request):
+    ver_user = User.objects.all()
+    return render(request, 'lista_users/lista.html', {'ver_user': ver_user})
+    
+def mudar_senha(request, id):
+    #Segurança para verificar se o usuario existe, se não, ele retorna um erro 404
+    user = get_object_or_404(User, id=id)
+    
+    if request.method == 'GET':
+        return render(request, 'lista_users/editar.html', {'user': user})
+    else:
+        #Recebe a nova senha
+        nova_senha = request.POST.get('senha')
+        if nova_senha:
+            user.set_password(nova_senha)
+            user.save()
+            return render(request, 'lista_users/editar.html', {'user': user , 'add': True})
+        else:
+            #Mensagem de erro(vai ser adicionada ainda)
+            return render(request, 'lista_users/editar.html', {'user': user , 'nao_add': True})
         
+def deletar_usuario(request, id):
+    #Estou buscando o produto pelo id, se nao existir, da erro 404
+    usuario = get_object_or_404(User, id = id)
+
+    if request.method == 'POST':
+        #Função para deletar o produto, produto esse que selecionei pelo id
+        usuario.delete()
+        return redirect('lista')  # Redireciona para o lista
+
+    return render(request, 'lista_users/remover.html', {'usuario': usuario})    
+
+def modificar_usuario(request, id):
+    # Isso aqui vai ver se o usuário existe, se não existir, retorna uma página 404
+    usuario = get_object_or_404(User, id=id)
+
+    if request.method == 'POST':
+        # Recebe os dados do form
+        usuario.first_name = request.POST.get('first_name')
+        usuario.last_name = request.POST.get('last_name')
+        usuario.username = request.POST.get('username')
+        usuario.email = request.POST.get('email')
+
+        usuario.save()
+
+        return render(request, 'lista_users/modificar_info.html', {'add': True, 'usuario': usuario})
+
+    return render(request, 'lista_users/modificar_info.html', {'usuario': usuario})
